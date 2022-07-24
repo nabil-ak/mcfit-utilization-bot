bb5c7e68095bef81283a3d71b7303143bb308494
Nabil <nabil.akir@outlook.de>
2022-07-24 04:35:29 +0200

    inital commit

---
+++ README.md
@@ -0,0 +1,39 @@
+<img src="https://upload.wikimedia.org/wikipedia/commons/3/3b/Logo_McFIT_GmbH.jpg" alt="icon" width="128" hight="128"/>
+
+
+# Mcfit-Utilization-Bot
+
+This discord bot will show you the current utilization of your gym.
+You will need your McFIT login credentials to use their API.
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install the requirement frameworks.
+
+```bash
+pip install -r requirements.txt
+```
+## Settings
+1. Change the ```EMAIL```, ```PASSWORD``` to your **login credentials**
+2. Set the right ```ID``` of your GYM.
+3. Create and set the ```token``` of your discord bot.
+
+```env
+EMAIL = ENTER_EMAIL
+PASSWORD = ENTER_PASSWORD
+STUDIO = THE_ID_OF_YOUR_STUDIO
+DISCORD_BOT_TOKEN = THE_TOKEN_OF_YOUR_DISCORD_BOT
+```
+## Usage
+1. Create a token for your discord bot in the discord developer dashboard.
+2. Change your .env file with your own data.
+3. Run the bot with ```python3 bot.py```.
+4. Use the ```/util```command to get the current utilization of your gym.
+
+
+## Example
+<img src="https://i.imgur.com/2mABMjF.png" alt="icon" width="265" hight="265"/>
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ bot.py
@@ -0,0 +1,106 @@
+import discord
+import requests
+import os
+from base64 import b64encode
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+EMAIL = os.environ['EMAIL']
+PASSWORD = os.environ['PASSWORD']
+STUDIO = os.environ['STUDIO']
+S = requests.Session()
+
+bot = discord.Bot() 
+
+async def login():
+    headers = {
+        'authority': 'my.mcfit.com',
+        'accept': '*/*',
+        'accept-language': 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
+        'authorization': f'Basic {(b64encode((EMAIL+":"+PASSWORD).encode("utf-8"))).decode("utf-8")}',
+        'origin': 'https://my.mcfit.com',
+        'referer': 'https://my.mcfit.com/login-register',
+        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
+        'x-nox-client-type': 'WEB',
+        'x-public-facility-group': 'MCFIT-2DBEBDE87C264635B943F583D13156C0',
+        'x-tenant': 'rsg-group',
+    }
+    
+    json_data = {
+        'username': EMAIL,
+        'password': PASSWORD,
+    }
+
+    r = S.post('https://my.mcfit.com/login', headers=headers, json=json_data)
+    r.raise_for_status()
+
+async def get_util():
+    headers = {
+        'authority': 'my.mcfit.com',
+        'accept': '*/*',
+        'accept-language': 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
+        'content-type': 'application/json',
+        'referer': 'https://my.mcfit.com/studio/cnNnLWdyb3VwOjE2NTUyODA0NTA%3D?v=1',
+        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
+        'x-ms-web-context': '/studio/cnNnLWdyb3VwOjE2NTUyODA0NTA%3D',
+        'x-nox-client-type': 'WEB',
+        'x-nox-web-context': 'v=1',
+        'x-public-facility-group': 'MCFIT-2DBEBDE87C264635B943F583D13156C0',
+        'x-tenant': 'rsg-group',
+    }
+    r = S.get(f'https://my.mcfit.com/nox/v1/studios/{STUDIO}/utilization', headers=headers)
+    if r.status_code != 200:
+        await login()
+        r = S.get(f'https://my.mcfit.com/nox/v1/studios/{STUDIO}/utilization', headers=headers)
+    r.raise_for_status()
+    return r.json()
+
+@bot.event
+async def on_ready():
+    print(f"We have logged in as {bot.user}")
+
+@bot.slash_command(description="Show the current Utilization")
+async def util(ctx):
+    try:
+        info = await get_util()
+        embed = discord.Embed(title="McFit Utilization", color=16774219) 
+        time = ""
+        status = ""
+        for item in info["items"]:
+            time+=f"{item['startTime']} -> {item['endTime']}\n"
+            if item['percentage']>80:
+                status+=f"🔴 {item['percentage']}%\n"
+            elif item['percentage']>40:
+                status+=f"🟡 {item['percentage']}%\n"
+            else:
+                status+=f"🟢 {item['percentage']}%\n"
+            if item['isCurrent']:
+                break
+
+        embed.add_field(name="Location", value=f"```Frankfurt Innenstadt```", inline=False)
+        embed.add_field(name="Time", value=f"```{time}```")
+        embed.add_field(name="Status", value=f"```{status}```")
+        embed.set_footer(text=f"{datetime.now().strftime('%d.%m.%Y %H:%M:%S')}")
+        await ctx.respond(embed=embed)
+    except Exception as e:
+        print(e)
+        await ctx.respond(f"Error")
+
+if __name__ == "__main__":       
+  bot.run(os.environ['DISCORD_BOT_TOKEN'])
+
+
